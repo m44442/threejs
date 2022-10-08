ac32310bb44762406eae5f141867ec1c74420b6c
TomoyaOka <web.toom.oka@gmail.com>
2022-10-08 23:02:41 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+//https://nogson2.hatenablog.com/entry/2017/12/24/193017
+
 import * as THREE from "three";
+import { gsap, Power4 } from "gsap";
 
 class App {
   /**
@@ -6,7 +9,7 @@ class App {
    */
   static get RENDERER_SETTING() {
     return {
-      clearColor: 0x000000,
+      clearColor: 0x111111,
       width: window.innerWidth,
       height: window.innerHeight,
     };
@@ -28,10 +31,10 @@ class App {
       fovy: 60,
       aspect: window.innerWidth / window.innerHeight,
       near: 0.1,
-      far: 20000.0,
+      far: 200.0,
       x: 0.0,
-      y: 2.0,
-      z: 30.0,
+      y: 0.0,
+      z: 8.0,
       lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
     };
   }
@@ -73,7 +76,6 @@ class App {
 
   _setScene() {
     this.scene = new THREE.Scene();
-    this.scene.fog = new THREE.Fog(0x000000, 20, 40);
   }
 
   _setCamera() {
@@ -93,27 +95,166 @@ class App {
   }
 
   _setMesh() {
-    const boxNumber = 12;
-    const center = 2;
-    this.group = new THREE.Group();
+    this.geometory = new THREE.PlaneGeometry(0.8, 10, 50, 80);
+    // this.geometory = new THREE.BoxGeometry(1, 10, 10, 10, 10, 10);
 
-    this.geometory = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    this.material = new THREE.MeshToonMaterial({ color: 0xffffff });
-    for (let i = 0; i < boxNumber; i++) {
-      for (let j = 0; j < boxNumber; j++) {
-        for (let k = 0; k < boxNumber; k++) {
-          this.mesh = new THREE.Mesh(this.geometory, this.material);
-          this.mesh.position.x = Math.cos(i) * 2;
-          this.mesh.position.y = Math.sin(j) * 2;
-          this.mesh.position.z = Math.sin(k) * 2;
-
-          this.array.push(this.mesh);
-          this.group.add(this.mesh);
-        }
+    this.material = new THREE.ShaderMaterial({
+      uniforms: {
+        uTime: { value: 0.0 },
+        uVertexTime: { value: 0.1 },
+      },
+      vertexShader: `
+      varying vec2 vUv;
+      uniform float uVertexTime;
+      vec3 mod289(vec3 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
       }
-    }
+      
+      vec2 mod289(vec2 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      
+      vec3 permute(vec3 x) {
+        return mod289(((x*34.0)+10.0)*x);
+      }
+      
+      float snoise(vec2 v)
+        {
+        const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                            0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                           -0.577350269189626,  // -1.0 + 2.0 * C.x
+                            0.024390243902439); // 1.0 / 41.0
+      // First corner
+        vec2 i  = floor(v + dot(v, C.yy) );
+        vec2 x0 = v -   i + dot(i, C.xx);
+      
+      // Other corners
+        vec2 i1;
+        //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+        //i1.y = 1.0 - i1.x;
+        i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+        // x0 = x0 - 0.0 + 0.0 * C.xx ;
+        // x1 = x0 - i1 + 1.0 * C.xx ;
+        // x2 = x0 - 1.0 + 2.0 * C.xx ;
+        vec4 x12 = x0.xyxy + C.xxzz;
+        x12.xy -= i1;
+      
+      // Permutations
+        i = mod289(i); // Avoid truncation effects in permutation
+        vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+          + i.x + vec3(0.0, i1.x, 1.0 ));
+      
+        vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+        m = m*m ;
+        m = m*m ;
+      
+      // Gradients: 41 points uniformly over a line, mapped onto a diamond.
+      // The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+      
+        vec3 x = 2.0 * fract(p * C.www) - 1.0;
+        vec3 h = abs(x) - 1.0;
+        vec3 ox = floor(x + 0.5);
+        vec3 a0 = x - ox;
+      
+      // Normalise gradients implicitly by scaling m
+      // Approximation of: m *= inversesqrt( a0*a0 + h*h );
+        m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+      
+      // Compute final noise value at P
+        vec3 g;
+        g.x  = a0.x  * x0.x  + h.x  * x0.y;
+        g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+        return 200.0 * dot(m, g);
+      }
+        void main() {
+          vUv = uv;
+          vec3 pos = position;
 
-    this.scene.add(this.group);
+       
+          pos.x = snoise(pos.xy) * uVertexTime * 1.0;
+          pos.y = snoise(normalize(pos.xy)) * uVertexTime * 1.0;
+          
+
+
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
+        }
+      `,
+      fragmentShader: `
+      precision mediump float;
+      uniform float uTime;
+
+      vec3 mod289(vec3 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      
+      vec2 mod289(vec2 x) {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+      }
+      
+      vec3 permute(vec3 x) {
+        return mod289(((x*34.0)+10.0)*x);
+      }
+      
+      float snoise(vec2 v)
+        {
+        const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                            0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                           -0.577350269189626,  // -1.0 + 2.0 * C.x
+                            0.024390243902439); // 1.0 / 41.0
+      // First corner
+        vec2 i  = floor(v + dot(v, C.yy) );
+        vec2 x0 = v -   i + dot(i, C.xx);
+      
+      // Other corners
+        vec2 i1;
+        //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+        //i1.y = 1.0 - i1.x;
+        i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+        // x0 = x0 - 0.0 + 0.0 * C.xx ;
+        // x1 = x0 - i1 + 1.0 * C.xx ;
+        // x2 = x0 - 1.0 + 2.0 * C.xx ;
+        vec4 x12 = x0.xyxy + C.xxzz;
+        x12.xy -= i1;
+      
+      // Permutations
+        i = mod289(i); // Avoid truncation effects in permutation
+        vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+          + i.x + vec3(0.0, i1.x, 1.0 ));
+      
+        vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+        m = m*m ;
+        m = m*m ;
+      
+      // Gradients: 41 points uniformly over a line, mapped onto a diamond.
+      // The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+      
+        vec3 x = 2.0 * fract(p * C.www) - 1.0;
+        vec3 h = abs(x) - 0.5;
+        vec3 ox = floor(x + 0.5);
+        vec3 a0 = x - ox;
+      
+      // Normalise gradients implicitly by scaling m
+      // Approximation of: m *= inversesqrt( a0*a0 + h*h );
+        m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+      
+      // Compute final noise value at P
+        vec3 g;
+        g.x  = a0.x  * x0.x  + h.x  * x0.y;
+        g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+        return 80.0 * dot(m, g);
+      }
+
+      
+      varying vec2 vUv;
+  void main() {
+       float offset = snoise(vUv + uTime);
+  gl_FragColor = vec4(vec3(offset), 1.0);
+    }
+      `,
+    });
+    this.mesh = new THREE.Mesh(this.geometory, this.material);
+
+    this.scene.add(this.mesh);
   }
 
   init() {
@@ -122,24 +263,23 @@ class App {
     this._setCamera();
     this._setMesh();
     this._setLight();
+
+    gsap.to(this.mesh.material.uniforms.uVertexTime, {
+      value: 1.2,
+      ease: "Power4.easeOut",
+      repeat: -1,
+      yoyo: true,
+      duration: 3,
+    });
   }
   render() {
     requestAnimationFrame(this.render);
-    this.group.rotation.x += 0.01;
-    this.group.rotation.y += 0.01;
+    this.mesh.material.uniforms.uTime.value += 0.004;
+    // this.mesh.material.uniforms.uVertexTime.value += 0.02;
 
-    const time = new Date().getTime() / 1000;
+    // this.mesh.rotation.x += 0.001;
+    // this.mesh.rotation.y += 0.001;
 
-    this.array.forEach((box, index) => {
-      if (index % 3) {
-        box.position.x += Math.sin(time - index) / 10;
-        box.position.y += Math.cos(time - index) / 10;
-        box.position.z += Math.cos(time - index) / 10;
-
-        box.scale.x += -Math.sin(time - index) / 100;
-        box.scale.y += -Math.cos(time - index) / 100;
-      }
-    });
     this.renderer.render(this.scene, this.camera);
   }
 }
